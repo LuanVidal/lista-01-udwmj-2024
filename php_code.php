ebe4a7c397ba4f41e11e3266862e47fc91316515
LuanVidal <65032589+LuanVidal@users.noreply.github.com>
2024-08-22 19:37:11 -0300

    Add files via upload

---
+++ php_code.php
@@ -0,0 +1,11 @@
+<?php
+// Defina os dois números
+$numero1 = 10;
+$numero2 = 20;
+
+// Some os números
+$soma = $numero1 + $numero2;
+
+// Exiba o resultado
+echo "A soma de $numero1 e $numero2 é $soma.";
+?>
